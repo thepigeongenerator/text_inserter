5da6f589200fd8ef4367061a156f88c0e367a0d6
Quinn <99677023+thepigeongenerator@users.noreply.github.com>
2024-10-28 10:28:13 +0100

    make start of project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::{path::Path, process::exit};
+
+const PATH_INPUTS: [&str; 1] = ["./test.html"];
+
+// error macro, which formats the printed text and exits with -1
+macro_rules! error {
+    ($($arg:tt)*) => {
+        print!("\x1b[91m");
+        print!($($arg)*);
+        print!("\x1b[0m\n");
+        exit(-1);
+    };
+}
+
 fn main() {
-    println!("Hello, world!");
+    if PATH_INPUTS.len() == 0 {
+        error!("no arguments were given!");
+    }
+
+    // loop through the different arguments
+    for arg in PATH_INPUTS {
+        let path = Path::new(arg); // create a new path from the argument
+
+        if path.exists() == false {
+            error!("could not find the file at '{}'", arg);
+        }
+    }
 }
